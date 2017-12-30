93972a42ba8abbad86f5ffd7510ee89ab60804dc
Andreas Gruhler <agruhl@gmx.ch>
2017-12-30 17:48:56 +0100

    add solidity coverage network

--- truffle.js
+++ truffle.js
@@ -10,6 +10,13 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    coverage: {
+      host: 'localhost',
+      network_id: '*', // eslint-disable-line camelcase
+      port: 8555,
+      gas: 0xfffffffffff,
+      gasPrice: 0x01,
     }
   }
 };
