b55e2ab13911be0e07681d012b0019178943e10c
Andreas Gruhler <andreas.gruhler@uzh.ch>
2018-01-12 15:00:46 +0100

    rewrites mongoid config file

--- fixtures/api/startrails.sh
+++ fixtures/api/startrails.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sed -ie "s/localhost/$MONGODB_IP/g" config/mongoid.yml
-sed -ie "s/MONGODB-USER/$MONGODB_USER/g" config/mongoid.yml
-sed -ie "s/MONGODB-PWD/$MONGODB_PWD/g" config/mongoid.yml
+perl -pe "BEGIN{undef $/;} s/hosts:.*?options:/hosts:\n        - $MONGODB_IP:27017\n      options:/smg" -i config/mongoid.yml
+sed -ie "s@user: '.*'@user: '$MONGODB_USER'@g" config/mongoid.yml
+sed -ie "s@password: '.*'@password: '$MONGODB_PWD'@g" config/mongoid.yml
 rails s -p 3000 -b 0.0.0.0
