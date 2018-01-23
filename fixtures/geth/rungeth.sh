6cb2594deb3d10eb3bd90b4c9065629898f9dfac
Andreas Gruhler <andreas.gruhler@uzh.ch>
2018-01-23 10:03:12 +0100

    rungeth

--- fixtures/geth/rungeth.sh
+++ fixtures/geth/rungeth.sh
@@ -9,11 +9,11 @@ then
  --networkid 4 --verbosity 6 \
  --rpccorsdomain "*" --rpc --rpcport 8545 --rpcaddr "0.0.0.0" --rpcapi="eth,net,web3,admin,personal" \
  --mine --minerthreads=1 --etherbase=0 \
- --bootnodes $2
+ --bootnodes "$2"
 else
  geth --fast --maxpeers 1 \
  --password pwd.txt --unlock "0,1" \
  --networkid 4 --verbosity 6 \
  --rpccorsdomain "*" --rpc --rpcport 8545 --rpcaddr "0.0.0.0" --rpcapi="eth,net,web3,admin,personal" \
- --bootnodes $2
+ --bootnodes "$2"
 fi
