f53a569e09c6d1036b7610584418675445162043
Andreas Gruhler <andreas.gruhler@uzh.ch>
2018-01-23 10:00:01 +0100

    initgeth

--- fixtures/geth/initgeth.sh
+++ fixtures/geth/initgeth.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sed -ie "s/ADDR0/$1/g" genesis.json
-sed -ie "s/ADDR1/$2/g" genesis.json
+sed -i -e "s/ADDR0/$1/g" genesis.json
+sed -i -e "s/ADDR1/$2/g" genesis.json
 
-geth init /genesis.json
+geth init genesis.json
