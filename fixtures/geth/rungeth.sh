7daa98f0ee91619928e46d9036b2620b2d8c0c7f
Andreas Gruhler <andreas.gruhler@uzh.ch>
2018-01-23 09:34:58 +0100

    geth verbosity

--- fixtures/geth/rungeth.sh
+++ fixtures/geth/rungeth.sh
@@ -2,18 +2,18 @@
 
 #--nodiscover
 
-if [ $1 -eq 1 ]
+if [ "$1" -eq 1 ]
 then
  geth --fast --maxpeers 1 \
  --password pwd.txt --unlock "0,1" \
- --networkid 4 --verbosity 3 \
+ --networkid 4 --verbosity 6 \
  --rpccorsdomain "*" --rpc --rpcport 8545 --rpcaddr "0.0.0.0" --rpcapi="eth,net,web3,admin,personal" \
  --mine --minerthreads=1 --etherbase=0 \
  --bootnodes $2
 else
  geth --fast --maxpeers 1 \
  --password pwd.txt --unlock "0,1" \
- --networkid 4 --verbosity 3 \
+ --networkid 4 --verbosity 6 \
  --rpccorsdomain "*" --rpc --rpcport 8545 --rpcaddr "0.0.0.0" --rpcapi="eth,net,web3,admin,personal" \
  --bootnodes $2
 fi
