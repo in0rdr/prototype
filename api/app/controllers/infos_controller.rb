daf0a91de1800ead35d6e29865eb9811a25bf4e5
Andreas Gruhler <andreas.gruhler@uzh.ch>
2018-02-06 14:04:13 +0100

    fix infos endpoint

--- api/app/controllers/infos_controller.rb
+++ api/app/controllers/infos_controller.rb
@@ -3,9 +3,9 @@ class InfosController < ApplicationController
   api! "Show server infos"
   def index
     infos = {
-      identity_contract: IDENTITY_ADDR,
-      mitigation_contract: MITGN_ADDR,
-      reputation_contract: REP_ADDR,
+      identity_contract: Decentral::IDENTITY_ADDR,
+      mitigation_contract: Decentral::MITGN_ADDR,
+      reputation_contract: Decentral::REP_ADDR,
       eth_rpc_url: Decentral::RPC_URL,
       ipfs_gateway: Decentral::IPFS_GATEWAY
     }
